af46639638a1867bf7941e4ce97705c524cb258b
bryan kennedy <bkennedy@smm.org>
2016-06-16 17:53:29 -0500

    Add Meteor package for users and basic template

---
+++ imports/ui/AccountsUIWrapper.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
+
+export default class AccountsUIWrapper extends Component {
+  componentDidMount() {
+    // Use Blaze to render the log in buttons
+    this.view = Blaze.render(Template.loginButtons,
+    ReactDOM.findDomNode(this.refs.container));
+  }
+
+  componentWillUnmount() {
+    // Clean up the Blaze view
+    Blaze.remove(this.view);
+  }
+
+  render() {
+    return <span ref="container" />;
+  }
+
+}
