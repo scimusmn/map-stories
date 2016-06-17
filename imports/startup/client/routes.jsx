702b470440ad1d682b6e979c36bad00f0d826767
bryan kennedy <bkennedy@smm.org>
2016-06-17 16:39:10 -0500

    Adding another basic route
    Just a skeleton path

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -3,11 +3,13 @@ import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 
 import App from '/imports/ui/App.jsx';
+import { Test } from '/imports/ui/Test.jsx';
 
 Meteor.startup(() => {
   render(
     <Router history={ browserHistory }>
       <Router path="/" component={ App } />
+      <Router path="/test" component={ Test } />
     </Router>,
     document.getElementById('render-target')
   );
---
+++ imports/ui/Test.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Test = () => <h3>Index</h3>;
