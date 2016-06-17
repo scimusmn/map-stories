eee289d7ee1079bdcf00119c3209ba84a82f77d2
bryan kennedy <bkennedy@smm.org>
2016-06-17 16:31:03 -0500

    Fixing import
    App is the default export so we don't want to use the curly braces.

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 
-import { App } from '../../ui/App.jsx';
+import App from '/imports/ui/App.jsx';
 
 Meteor.startup(() => {
   render(
