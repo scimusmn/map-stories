ef5189c9cb62d0e016420d209100bb5fd23b7d6b
bryan kennedy <bkennedy@smm.org>
2016-06-22 22:47:05 -0500

    Adding component for listing places

---
+++ imports/ui/components/ListPlaces.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class ListPlaces extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { places } = this.props;
+    return (
+      <div>
+        <p>List of Places</p>
+        <ul>
+          {places.map(place => (
+            <li> {place.name} </li>
+          ))}
+        </ul>
+      </div>
+    );
+
+  }
+
+}
+
+ListPlaces.propTypes = {
+  places: React.PropTypes.array,
+};
+
+ListPlaces.contextTypes = {
+  router: React.PropTypes.object,
+};
