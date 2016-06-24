0dcac87cb91ebe96eaf476cc512e85c63f19c112
bryan kennedy <bkennedy@smm.org>
2016-06-24 13:34:25 -0500

    Create a slug for each place
    This gives us a URL for the route

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,10 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { Places } from '../../api/places/places.js';
+import _ from 'lodash';
 
 // If the database is empty on server start, create some sample data.
 Meteor.startup(() => {
   Places.remove({});
   if (Places.find().count() === 0) {
+
     const data = [
       {
         name: 'Pike Island',
@@ -19,8 +21,8 @@ Meteor.startup(() => {
     ];
 
     data.forEach((place) => {
+      place.slug = _.kebabCase(place.name);
       Places.insert(place);
-
     });
   }
 });
