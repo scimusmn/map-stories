4870607dbbc2578c518d253f2ea3e18516537c26
bryan kennedy <bkennedy@smm.org>
2016-06-27 12:25:33 -0500

    Removing old navigation content
    This was left over from testing.

--- imports/ui/components/navigation.jsx
+++ imports/ui/components/navigation.jsx
@@ -4,7 +4,5 @@ import { IndexLink, Link } from 'react-router';
 export const Navigation = () => (
   <ul>
     <li><IndexLink to="/" activeClassName="active">Index</IndexLink></li>
-    <li><IndexLink to="/one" activeClassName="active">Page one</IndexLink></li>
-    <li><IndexLink to="/two" activeClassName="active">Page two</IndexLink></li>
   </ul>
 );
