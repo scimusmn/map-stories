bb48834fd7ed8a7e1734dd599d4fca11183c8e64
bryan kennedy <bkennedy@smm.org>
2016-06-27 14:04:43 -0500

    Restructuring the singlePlace component
    Breaking this out of the shortcut syntax so that we can build some
    methods for modifying the plage display.

--- imports/ui/components/SinglePlace.jsx
+++ imports/ui/components/SinglePlace.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import { Link } from 'react-router';
 import { Loading } from '../../ui/components/loading.js';
 
-const renderPlace = (places) => (
-  <div>
-    {places.name}
-  </div>
-);
-
-export const SinglePlace = ({ places }) => (
-  places.length > 0 ? <div className="documents-list">
-    {places.map((place) => (
-      <div key={ place._id }>{place.name}</div>
-    ))}
-  </div> :
-  <div>No places yet.</div>
-);
+export class SinglePlace extends React.Component {
+  render() {
+    const places = this.props.places;
+    return (
+      places.length > 0 ? <div className="documents-list">
+      {places.map((place) => (
+          <div key={ place._id }>{place.name}</div>
+        ))}
+      </div> :
+      <div>No places yet.</div>
+    );
+  }
+}
