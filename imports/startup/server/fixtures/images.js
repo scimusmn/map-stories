d89cfa738f5925c542e481e3d46fb672f34f9b2e
bryan kennedy <bkennedy@smm.org>
2016-07-19 15:22:59 -0500

    Initial image fixture
    Just some very basic data

---
+++ imports/startup/server/fixtures/images.js
@@ -0,0 +1,27 @@
+import { Meteor } from 'meteor/meteor';
+import { Places } from '../../../api/images/images.js';
+import _ from 'lodash';
+
+// If the database is empty on server start, create some sample data.
+
+Meteor.startup(() => {
+  Images.remove({});
+  if (Images.find().count() === 0) {
+
+    const data = [
+      {
+        place: 'Coon Rapids',
+        imgTitle: 'Dam',
+      },
+      {
+        place: 'Coon Rapids',
+        imgTitle: 'Men Lounging',
+      },
+    ];
+
+    data.forEach((image) => {
+      image.slug = _.kebabCase(image.name);
+      Images.insert(image);
+    });
+  }
+});
