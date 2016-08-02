d4db7a52174b482f76412b20690fc8d761cd9e7f
bryan kennedy <bkennedy@smm.org>
2016-08-02 18:28:14 -0500

    Adjust animation to expand from map point
    Images start small from the map point, fade in, and get bigger as the
    move to their non-overlapping spot.

--- imports/ui/helpers/chart.js
+++ imports/ui/helpers/chart.js
@@ -41,85 +41,79 @@ d3Chart._drawMap = function (el, props, state) {
       .precision(0.1);
 
   // Draw county boundaries in dev mode
+  //noinspection JSUnresolvedVariable
   if (Meteor.settings.public.dev == 'true') {
     drawCounties(el, props, projection);
   }
 
-  _.each(_.shuffle(places), function (place, i) {
-    setTimeout(
-      function () {
-        // Find the images for this location
-        const placeImages = _.filter(images, function (o) {
-          if (o.place == place.name) {
-            return true;
-          }
-        });
+  _.each(places, function (place, i) {
+    // Find the images for this location
+    const placeImages = _.filter(images, function (o) {
+      if (o.place == place.name) {
+        return true;
+      }
+    });
 
-        // Select a random image
-        const placeImage = _.sample(placeImages);
+    // Select a random image
+    const placeImage = _.sample(placeImages);
 
-        // Image width
-        const maxWidth = 200;
+    // Image width
+    const maxWidth = 200;
 
-        // Line delay
-        // Time for the image to fade in, before drawing line
-        const picDur = 400;
+    // Line animation duration
+    const lineDur = 200;
 
-        // Line animation duration
-        const lineDur = 1200;
+    // Stagger delay for each location
+    let stagger = 200 * (i + 1 - 0.9);
+    setTimeout(function () {
+      // Draw the location labels and images
+      drawPlace(projection, place, placeImage, maxWidth, lineDur);
 
-        // Draw the location labels and images
-        drawPlace(projection, place, placeImage, maxWidth, picDur);
+      // Draw map dots, for each location
+      drawLine(el, projection, place, maxWidth, lineDur);
 
-        // Wait for the images to load before drawing map points
-        setTimeout(function () {
-          // Draw map dots, for each location
-          drawLine(el, projection, place, maxWidth, i, lineDur);
-
-          // Draw map dots, for each location
-          drawDot(el, projection, place, maxWidth, i, lineDur);
-
-        }, picDur);
-      },
-    1000 * i);
+      // Draw map dots, for each location
+      drawDot(el, projection, place, maxWidth);
+    }, stagger);
 
   });
 
   $('.place-label').click(function (e) {
+    e.preventDefault();
     var $clickedId = $(this).attr('id');
     window.location.href = '/place/' + $clickedId;
   });
-
-  var items = document.getElementsByClassName('place-label')[0];
 };
 
 /**
  * Draw a line between the dot and the picture
  *
- * This helps people know which picture goes with which spot
+ * The line helps people know which picture goes with which spot
  *
- * @param el Parent SVG element
- * @param projection
- * @param place
- * @param maxWidth
+ * @param {object} el Parent SVG element
+ * @param {object} projection D3 map projection
+ * @param {object} place Meteor data object for the place
+ * @param {int} maxWidth Width of the image circle.
+ *              Used here for calculating animation centers
+ * @param dur {int} Line drawing animation duration
  */
-function drawLine(el, projection, place, maxWidth, i, dur) {
+function drawLine(el, projection, place, maxWidth, dur) {
   var placeGroup = d3.select(el).selectAll('.d3-points')
     .append('g')
     .classed('place-line', true);
   var line = linePoints(projection, place, maxWidth);
-
   placeGroup.append('line')
-    .attr('x1', line.x2)
-    .attr('y1', line.y2)
-    .attr('x2', line.x2)
-    .attr('y2', line.y2)
+    .attr('x1', line.x1)
+    .attr('y1', line.y1)
+    .attr('x2', line.x1)
+    .attr('y2', line.y1)
+    .attr('opacity', 0)
     .classed('map-line', true)
       .transition()
         .duration(dur)
-        .delay(i * 30)
-        .attr('x1', line.x1)
-        .attr('y1', line.y1);
+        .attr('x1', line.x2)
+        .attr('y1', line.y2)
+        .attr('opacity', 1);
 }
 
 function linePoints(projection, place, maxWidth) {
@@ -137,26 +131,21 @@ function linePoints(projection, place, maxWidth) {
  * We draw this separately since we don't want any other transformations to
  * move the lat/long based map dot.
  *
- * @param  {object} el Top level react DOM parent for our SVG elements
- * @param  {function} projection D3 map projection object
- * @param  {object} place Place object data from Meteor
+ * @param {object} el Top level react DOM parent for our SVG elements
+ * @param {function} projection D3 map projection object
+ * @param {object} place Place object data from Meteor
+ * @param {int} maxWidth Width of the image circle.
+ *              Used here for calculating animation centers
  */
-function drawDot(el, projection, place, maxWidth, i, dur) {
+function drawDot(el, projection, place, maxWidth) {
   var placeGroup = d3.select(el).selectAll('.d3-points')
     .append('g')
     .classed('place-dot', true);
   placeGroup.append('circle')
     .attr('r', '5px')
     .classed('map-dot', true);
-
   var line = linePoints(projection, place, maxWidth);
-  placeGroup.attr('transform', 'translate(' + line.x2 + ',' + line.y2 + ')');
-  placeGroup
-    .transition()
-    .duration(dur)
-    .delay(i * 30)
-    .attr('transform', 'translate(' + line.x1 + ',' + line.y1 + ')');
-
+  placeGroup.attr('transform', 'translate(' + line.x1 + ',' + line.y1 + ')');
 }
 
 /**
@@ -168,15 +157,18 @@ function drawDot(el, projection, place, maxWidth, i, dur) {
  * @param  {object} place Place object data from Meteor
  * @param  {object} placeImage TODO: add description
  * @param  {object} maxWidth Maximum width for the image
- * @param  {int} picDur Miliseconds for the fade in animation
+ * @param  {int} picDur Milliseconds for the fade in animation
  */
 function drawPlace(projection, place, placeImage, maxWidth, picDur) {
-  let translateX = projection([place.long, place.lat])[0] + place.offsetX;
-  let translateY = projection([place.long, place.lat])[1] + place.offsetY;
+  var center = {};
+  center.x1 = projection([place.long, place.lat])[0];
+  center.y1 = projection([place.long, place.lat])[1];
+  center.x2 = projection([place.long, place.lat])[0] + place.offsetX;
+  center.y2 = projection([place.long, place.lat])[1] + place.offsetY;
 
   // Parent group div for picture and the labels
-  var xStyle = ['left', translateX + 'px'];
-  var yStyle = ['top', translateY + 'px'];
+  var xStyle = ['left', center.x1 + 'px'];
+  var yStyle = ['top', center.y1 + 'px'];
 
   var $newDiv = $('<div/>')
    .attr('id', place.slug)
@@ -194,22 +186,32 @@ function drawPlace(projection, place, placeImage, maxWidth, picDur) {
   var $placeLabelBackground = $('<div/>')
     .addClass('place-label-background')
     .css('background-image', 'url(' + backgroundImage + ')')
-    .css('width', displayDimension + 'px')
-    .css('height', displayDimension + 'px');
+    .css('width', '0px')
+    .css('height', '0px');
 
-  var $placeLabelText = $('<div/>')
-    .addClass('place-label-text')
-    .html(place.name);
-
-  $placeLabelBackground.append($placeLabelText);
   $newDiv.append($placeLabelBackground);
   $('.Chart').append($newDiv);
 
   $newDiv.animate({
     opacity: 1,
-  }, picDur, function() {
-    console.log('Animation complete');
+    left: center.x2,
+    top: center.y2,
+  }, picDur, function () {
+    var $placeLabelText = $('<div/>')
+      .addClass('place-label-text')
+      .html(place.name);
+    $placeLabelBackground.append($placeLabelText);
+    $placeLabelText
+      .animate({
+        opacity: 1,
+      }, 500);
   });
+
+  $placeLabelBackground.animate({
+    width: displayDimension + 'px',
+    height: displayDimension + 'px',
+  }, picDur);
+
 }
 
 /**
@@ -218,6 +220,7 @@ function drawPlace(projection, place, placeImage, maxWidth, picDur) {
  * We use this in development to align the raster and vector layers.
  *
  * @param  {object} el Top level react DOM parent for our SVG elements
+ * @param  {object} props React component properties, contains chart details
  * @param  {object} projection D3 map projection object
  */
 function drawCounties(el, props, projection) {
@@ -234,6 +237,7 @@ function drawCounties(el, props, projection) {
   var path = d3.geo.path().projection(projection);
   var mapG = d3.select(el).selectAll('.d3-map');
   d3.json('/data/counties.json', function (error, counties) {
+    //noinspection JSUnresolvedVariable
     mapG.append('path')
     .datum(topojson.feature(counties, counties.objects.counties))
     .attr('d', path)
