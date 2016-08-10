8c69309c3a2932eb0648e88a29b5afc7cb606081
bryan kennedy <bkennedy@smm.org>
2016-08-10 14:17:25 -0500

    Add a imageFiles collection
    This is generated from the list of files saved in the file system.

---
+++ imports/api/imageFiles/imageFiles.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+
+export const ImageFiles = new Mongo.Collection('ImageFiles');
--- imports/startup/server/files.js
+++ imports/startup/server/files.js
@@ -1,3 +1,5 @@
+import { Meteor } from 'meteor/meteor';
+import { ImageFiles } from '/imports/api/imageFiles/imageFiles';
 import fs from 'fs';
 import _ from 'lodash';
 import mime from 'mime';
@@ -5,17 +7,39 @@ import mime from 'mime';
 // Path to our images folder
 const collectionImages = process.env.PWD + '/public/images/collection/';
 
-// Read the files in the collections folder and build a
-fs.readdir(collectionImages, function (err, images) {
-  _.each(images, function(image) {
+// Read the files in the collections folder and build a collection
+Meteor.startup(() => {
 
-    const mimeType = mime.lookup(collectionImages + image);
-    if (mimeType != 'image/jpeg') {
-      console.log('Found a non-JPG file');
-      console.log(image);
-    }
+  // Clear out the ImageFiles collection before reading from the directory
+  ImageFiles.remove({});
 
-  });
+  fs.readdir(
+    collectionImages,
+    // Handle the async file read process inside of Meteor's fiber
+    Meteor.bindEnvironment(
+      function (err, imageFiles) {
+
+        /**
+         * If the image file is an allowed file type, insert it into the
+         * ImageFiles db collection
+         */
+        _.each(imageFiles, function (imageFile) {
+          const allowedTypes = [
+            'image/jpeg',
+            'image/png',
+          ];
+          const mimeType = mime.lookup(collectionImages + imageFile);
+          if (_.includes(allowedTypes, mimeType)) {
+            const imageFileObject = { filename: imageFile };
+            ImageFiles.insert(imageFileObject);
+          } else {
+            console.log(mimeType);
+          }
+
+        });
+
+      }
+    )
+  );
 
 });
-
