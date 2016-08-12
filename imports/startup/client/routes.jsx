40b48a3ed9283cf468562fb83948b24ee8087eb9
bryan kennedy <bkennedy@smm.org>
2016-08-12 12:45:50 -0500

    ES6 style cleanup

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -5,19 +5,17 @@ import AppContainer from '../../ui/containers/AppContainer.jsx';
 import ListPlacesContainer from '../../ui/containers/ListPlacesContainer.jsx';
 import PlaceContainer from '../../ui/containers/PlaceContainer.jsx';
 import AdminContainer from '../../ui/containers/AdminContainer';
-
-import { App } from '../../ui/layouts/App.jsx';
-import { Index } from '../../ui/components/index.jsx';
-
 import { NotFound } from '../../ui/components/notFound.jsx';
 
-export const renderRoutes = () => (
-  <Router history={ browserHistory }>
-    <Route path="/" component={ AppContainer }>
-      <IndexRoute component={ ListPlacesContainer } />
-      <Route path="/place/:slug" component={ PlaceContainer } />
-      <Route path="/admin" component={ AdminContainer } />
+const renderRoutes = () => (
+  <Router history={browserHistory}>
+    <Route path="/" component={AppContainer}>
+      <IndexRoute component={ListPlacesContainer} />
+      <Route path="/place/:slug" component={PlaceContainer} />
+      <Route path="/admin" component={AdminContainer} />
     </Route>
-    <Route path="*" component={ NotFound } />
+    <Route path="*" component={NotFound} />
   </Router>
 );
+
+export default renderRoutes;
