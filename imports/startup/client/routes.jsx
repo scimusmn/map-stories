33a1f2f26ab8e2c437b0c734016ba24c08ede095
bryan kennedy <bkennedy@smm.org>
2016-08-12 12:41:35 -0500

    Removing unused import

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import AppContainer from '../../ui/containers/AppContainer.jsx';
