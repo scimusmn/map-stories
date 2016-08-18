c75955a40982ab95f12f6960371397cc1a9a4c7a
bryan kennedy <bkennedy@smm.org>
2016-08-17 21:38:40 -0500

    Methods for updating and inseting new images

--- imports/api/images/methods.js
+++ imports/api/images/methods.js
@@ -1,11 +1,21 @@
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { Images } from './images';
 
-const updateImage = new ValidatedMethod({
+export const updateImage = new ValidatedMethod({
   name: 'updateImage',
   validate: null,
-  run() {
-    console.log('Running the method');
+  run({ _id, updatedImage }) {
+    Images.update(_id, {
+      $set: updatedImage,
+    });
   },
+
 });
 
-export default updateImage;
+export const submitImage = new ValidatedMethod({
+  name: 'submitImage',
+  validate: null,
+  run({ updatedImage }) {
+    Images.insert(updatedImage);
+  },
+});
