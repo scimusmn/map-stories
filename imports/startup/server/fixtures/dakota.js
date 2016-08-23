a2b6b60e59bfd1bb890d288f084cdf71c211b7c1
bryan kennedy <bkennedy@smm.org>
2016-08-23 12:39:54 -0500

    More Dakota locations

--- imports/startup/server/fixtures/dakota.js
+++ imports/startup/server/fixtures/dakota.js
@@ -10,13 +10,37 @@ Meteor.startup(() => {
 
     const data = [
       {
-        lat: 44.973497,
-        long: -93.237940,
+        lat: 44.885965,
+        long: -93.162233,
         place: 'Mendota',
         englishPlaceName: 'Mendota',
         dakotaPlaceName: 'Bdote',
         nameFilename: 'mendota-name.mp4',
         explainFilename: 'mendota-explain.mp4',
+        offsetX: 0,
+        offsetY: 0,
+      },
+
+      {
+        lat: 44.805654,
+        long: -93.220164,
+        place: 'Mendota',
+        englishPlaceName: 'Black Dog\'s village',
+        dakotaPlaceName: 'Ṡunkasapa',
+        nameFilename: 'black-dog-name.mp4',
+        explainFilename: 'black-dog-explain.mp4',
+        offsetX: 0,
+        offsetY: 0,
+      },
+
+      {
+        lat: 44.844824,
+        long: -93.170841,
+        place: 'Mendota',
+        englishPlaceName: 'Pilot Knob',
+        dakotaPlaceName: 'Oheyawahi',
+        nameFilename: 'pilot-knob-name.mp4',
+        explainFilename: 'pilot-knob-explain.mp4',
         offsetX: 0,
         offsetY: 0,
       },
